bedc56b6a69a1de535a38c5882041ebc9edc2b12
Bernardo Serrano <bernardo.serrano.sanguinetti@gmail.com>
2024-04-02 09:50:42 -0300

    Add files via upload

---
+++ valor1 = int(input('Digite um numero'));.py
@@ -0,0 +1,25 @@
+# valor1 = int(input('Digite um numero: '));
+# valor2 = int(input('Digite outro numero: '));
+# valor3 = int(input('Digite outro numero: '));
+# medianota = (valor1 + valor2 + valor3) / 3;
+# print("A média é", medianota);
+
+
+
+
+
+# salariohora = float(input("Quanto você ganha por hora: "));
+# tempotrabalho = float(input('Quantas horas você trabalhou no mês: '));
+# salarioreal = salariohora * tempotrabalho;
+# print("Seu salário é: ", salarioreal);
+
+
+
+pesopeixes = float(input('Quantos quilos de peixe você pescou: '));
+valormulta = 4;
+if pesopeixes > 50:
+ print('Você pagará uma multa de:',valormulta * (pesopeixes - 50));
+else:
+ print('Você não pagará multa');
+                  
+                
